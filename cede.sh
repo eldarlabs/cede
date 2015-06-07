bdf06fae2a8aabecaf86448caee9966909b4c0d4
Arlair <arlair@yahoo.com>
2015-06-08 01:07:50 +0800

    wip

--- cede.sh
+++ cede.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-echo 'cede test'
+# Treat unset variables as an error
+set -o nounset
+
+__ScriptVersion="0.1.0"
+__ScriptName="cede.sh"
+
+main() {
+	#usage
+	
+  #$NEW_USER_NAME
+
+	sudo gpasswd -a newu sudo
+}
+
+main
+
+usage() {
+    cat << EOT
+
+  Usage :  ${__ScriptName} [options]
+
+  Examples:
+    - ${__ScriptName}
+
+EOT
+}
